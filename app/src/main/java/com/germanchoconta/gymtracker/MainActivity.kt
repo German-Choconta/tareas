1d2a7f4fc761b59567feed35c4c0d215a6a9e259
German-Choconta <german_choconta@outlook.com>
2026-08-24 22:59:15 -0500

    feat: wire workout repository into app

--- app/src/main/java/com/germanchoconta/gymtracker/MainActivity.kt
+++ app/src/main/java/com/germanchoconta/gymtracker/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import com.germanchoconta.gymtracker.data.local.ExerciseRepository
 import com.germanchoconta.gymtracker.data.local.GymTrackerDatabase
 import com.germanchoconta.gymtracker.data.local.RoutineRepository
+import com.germanchoconta.gymtracker.data.local.WorkoutRepository
 import com.germanchoconta.gymtracker.ui.GymTrackerApp
 import com.germanchoconta.gymtracker.ui.theme.GymTrackerTheme
 
@@ -15,12 +16,18 @@ class MainActivity : ComponentActivity() {
         val database = GymTrackerDatabase.build(applicationContext)
         val exerciseRepository = ExerciseRepository(database.exerciseDao(), database.muscleDao())
         val routineRepository = RoutineRepository(database.routineDao())
+        val workoutRepository = WorkoutRepository(
+            workoutDao = database.workoutDao(),
+            routineDao = database.routineDao(),
+            exerciseDao = database.exerciseDao(),
+        )
 
         setContent {
             GymTrackerTheme {
                 GymTrackerApp(
                     exerciseRepository = exerciseRepository,
                     routineRepository = routineRepository,
+                    workoutRepository = workoutRepository,
                 )
             }
         }

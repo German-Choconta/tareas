f1482931e05d85d8fbc69eda15da97fa449104b4
German-Choconta <german_choconta@outlook.com>
2026-08-25 23:06:45 -0500

    Fix Wear lazy column DSL import

--- wear/src/main/java/com/germanchoconta/gymtracker/wear/MainActivity.kt
+++ wear/src/main/java/com/germanchoconta/gymtracker/wear/MainActivity.kt
@@ -24,7 +24,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
-import androidx.wear.compose.foundation.lazy.item
 import androidx.wear.compose.material3.Button
 import androidx.wear.compose.material3.MaterialTheme
 import androidx.wear.compose.material3.Text
